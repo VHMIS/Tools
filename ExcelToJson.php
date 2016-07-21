e88dee240736e29934bd6544ca138e8779fdccd0
Lê Nhật Anh <lenhatanh86@gmail.com>
2016-07-21 15:15:51 +0700

    Update ExcelToJson.php

--- ExcelToJson.php
+++ ExcelToJson.php
@@ -58,3 +58,40 @@ for ($i = 2; $i <= 531; $i++) {
 
 file_put_contents('college.json', json_encode($data));
 */
+
+/*
+<?php
+
+define('PHPEXCEL_ROOT', '/WebServer/PHPExcel/Classes/');
+include PHPEXCEL_ROOT . 'PHPExcel/Autoloader.php';
+
+$objReader = \PHPExcel_IOFactory::createReader('Excel5');
+$objPHPExcel = $objReader->load('dm.xls');
+$objPHPExcel->setActiveSheetIndex(0);
+
+$areas = [
+    'KV 1' => '1',
+    'KV 2-NT' => '2NT',
+    'KV 2' => '2',
+    'KV 3' => '3'
+];
+
+$data = [];
+for ($i = 2; $i <= 3702; $i++) {
+    $code_p = $objPHPExcel->getActiveSheet()->getCell('B' . $i)->getValue();
+    $code_s = $objPHPExcel->getActiveSheet()->getCell('F' . $i)->getValue();
+    $name_s = $objPHPExcel->getActiveSheet()->getCell('G' . $i)->getValue();
+    $area_c = $objPHPExcel->getActiveSheet()->getCell('J' . $i)->getValue();
+    $area_c = $areas[$area_c];
+    
+    
+    $data[$code_p][$code_s] = [
+        'name' => $name_s,
+        'area' => $area_c
+    ];
+}
+
+foreach($data as $pcode => $schools) { 
+    file_put_contents('JSONCreate/pt_' . $pcode . '.json', json_encode($schools, JSON_PRETTY_PRINT));
+}
+*/
